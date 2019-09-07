6690f45d0e1b87924d761fef799dd0eb9f2b8edb
dyah <dyahnrchls@gmail.com>
2019-09-07 20:41:18 +0700

    add transaction ID

--- src/screens/CallBillScreen.js
+++ src/screens/CallBillScreen.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 import {View, Text, Image} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+
 import OrderButton from '../components/OrderButton'
 
 export default class CallBillScreen extends Component{
 
     static navigationOptions = {
         header: null
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            transactionId: 0,
+            tableNumber: 0,
+        }
+    }
+    
+    getTableNumber = async () => {
+        try {
+            const tableNum = await AsyncStorage.getItem('TABLE_NUMBER')
+            const id = await AsyncStorage.getItem('TRANSACTION_ID')
+            
+            this.setState({
+                tableNumber: tableNum,
+                transactionId: id,
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    componentDidMount(){
+        this.getTableNumber()
     }
 
     render(){
@@ -19,7 +47,7 @@ export default class CallBillScreen extends Component{
                     TO PROCEED WITH THE PAYMENT 
                 </Text>
                 <Text style={{fontSize:50}}>
-                    # 20
+                    # {this.state.transactionId}
                 </Text>
                 <OrderButton text="BACK TO HOME" button={() => this.props.navigation.navigate('MainScreen')}/>
             </View>
